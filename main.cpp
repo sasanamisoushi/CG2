ed97dc3397851cf32e81f52b0318117638f2c45d
颯志 笹浪 <k024g0014@g.neec.ac.jp>
2025-04-15 11:17:24 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<Windows.h>
+
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//出力ウィンドへの文字 
+	OutputDebugStringA("Hello,DirectX!\n");
+	return 0;
+}
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージを処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
