6dc07a08f0ee4c2a06d3b941f56e87a3cfd9f5d1
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-04-16 17:15:28 +0900

    DirectX12を使う前準備のアダプタなどを入れた

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 #include <filesystem>
 #include <fstream>
 #include <chrono>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
 
 
 
@@ -117,6 +123,66 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//出力ウィンドへの文字 
 	Log(logStream, "Hello,DirectX!\n");
 	Log(logStream, ConvertString(std::format(L"windth:{}\n", kClientWidth)));
+
+	//IDXGIファクトリーの生成
+	IDXGIFactory7 *dxgiFactory = nullptr;
+
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+
+	assert(SUCCEEDED(hr));
+
+	//使用するアダプタ用の変数
+	IDXGIAdapter4 *useAdapter = nullptr;
+
+	//良い順にアダプタを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, 
+		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, 
+		IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; ++i) {
+
+		//アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+
+		//ソフトウェアアダプタ出なければ採用
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+
+			//採用したアダプタの情報をログに出力
+			Log(logStream, ConvertString(std::format(L"Use Adapater:{}\n", adapterDesc.Description)));
+			break;
+		}
+
+		useAdapter = nullptr;
+	}
+
+	//適切なアダプタが見つからなかったので起動できない
+	assert(useAdapter != nullptr);
+
+
+	ID3D12Device *device = nullptr;
+	//機能レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0 
+	};
+	
+	const char *featureLevelStrings[] = { "12.2","12.1","12.0" };
+
+	//高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(featureLevels); ++i) {
+		//採用したアダプターでデバイス生成
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		//指定した機能レベルでデバイスを生成できたか確認
+		if (SUCCEEDED(hr)) {
+			//生成できたのでログ出力を行ってループを抜ける
+			Log(logStream, std::format("FeatureLevel:{}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+
+	//デバイスの生成が上手くいかなかったので起動できない
+	assert(device != nullptr);
+	Log(logStream, "Complete create D3D12Device!!!\n");
 
 	MSG msg{};
 	while (msg.message !=WM_QUIT) {
