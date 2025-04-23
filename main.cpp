970949dceac222f56c17c916678b7707e5888d32
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-04-23 16:58:08 +0900

    エラーの表示

--- main.cpp
+++ main.cpp
@@ -152,11 +152,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		wc.hInstance,
 		nullptr);
 
+#ifdef _DEBUG
+	ID3D12Debug1 *debugController = nullptr;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
+		//デバッグレイヤーを有効化する
+		debugController->EnableDebugLayer();
+		//さらにGPU側でもチェックを行うようにする
+		debugController->SetEnableGPUBasedValidation(TRUE);
+	}
+#endif 
+
 	//ウインドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
 	//出力ウィンドへの文字 
 	Log(logStream, "Hello,DirectX!\n");
-	Log(logStream, ConvertString(std::format(L"windth:{}\n", kClientWidth)));
+	Log(logStream, ConvertString(std::format(L"windth:{}\n", kClientWidth))); 
+
 
 	//IDXGIファクトリーの生成
 	IDXGIFactory7 *dxgiFactory = nullptr;
@@ -217,6 +228,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//デバイスの生成が上手くいかなかったので起動できない
 	assert(device != nullptr);
 	Log(logStream, "Complete create D3D12Device!!!\n");
+
+#ifdef _DEBUG
+
+	ID3D12InfoQueue *infoQueue = nullptr;
+	if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue)))) {
+		//ヤバいエラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+		//エラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR,true);
+		//警告時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+		
+		//抑制するメッセージのID
+		D3D12_MESSAGE_ID denyIds[] = {
+			D3D12_MESSAGE_ID_RESOURCE_BARRIER_INVALID_COMMAND_LIST_TYPE
+		};
+
+		//抑制するレベル
+		D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
+		D3D12_INFO_QUEUE_FILTER filter{};
+		filter.DenyList.NumIDs = _countof(denyIds);
+		filter.DenyList.pIDList = denyIds;
+		filter.DenyList.NumSeverities = _countof(severities);
+		filter.DenyList.pSeverityList = severities;
+		//指定したメッセージの表示を抑制する
+		infoQueue->PushStorageFilter(&filter);
+		//解放
+		infoQueue->Release();
+	}
+#endif
 
 	//コマンドキューを生成
 	ID3D12CommandQueue *commandQueue = nullptr;
