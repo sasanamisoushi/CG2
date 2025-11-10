8d51491c51f567ea1e96418a888fa10e133a6399
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-11-10 10:42:08 +0900

    訂正

--- project/engine/base/main.cpp
+++ project/engine/base/main.cpp
@@ -714,9 +714,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	winApp = new WinApp();
 	winApp->Initialize();
 
-	
-
-
 #ifdef _DEBUG
 	Microsoft::WRL::ComPtr<ID3D12Debug1> debugController = nullptr;
 	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
